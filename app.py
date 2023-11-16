c90ad3feaa17aa0733835c4dc448d01a80a13a5c
gregorymark254 <gregorymark254@gmail.com>
2023-11-16 14:09:36 +0300

    added login forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dcdf1e982461e411b1d6627716bef3bf'
 
 posts = [
     {
@@ -17,6 +20,7 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -26,6 +30,26 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login successful for {form.email.data}!', 'success')
+        return redirect(url_for('home'))
+    else:
+        flash(f'Login unsuccessful for {form.email.data}!', 'danger')
+    return render_template('login.html', title="Login", form=form)
 
 
 if __name__ == '__main__':
